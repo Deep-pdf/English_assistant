1e80d1abf212b76e2afa7102e5224bc5e0ff332c
Soumodeep <soumodeepsantra5@gmail.com>
2026-01-05 21:16:02 +0530

    landing page done

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-        document.getElementById('start-link').addEventListener('click', function(e) {
-            e.preventDefault();
-            const circle = document.querySelector('.H-circle');
-            circle.classList.add('expand-active');
-            setTimeout(() => {
-                window.location.href = 'conversation.html';
-            }, 700);
-        });
+document.getElementById('start-link').addEventListener('click', function (e) {
+    e.preventDefault();
+    const circle = document.querySelector('.H-circle');
+    circle.classList.add('expand-active');
+    setTimeout(() => {
+        window.location.href = 'conversation.html';
+    }, 700);
+});
+
+const toogle=document.getElementById('themeToogle');
+
+toogle.addEventListener("change", ()=>{
+    document.body.classList.toggle("dark",toogle.checked);
+});
